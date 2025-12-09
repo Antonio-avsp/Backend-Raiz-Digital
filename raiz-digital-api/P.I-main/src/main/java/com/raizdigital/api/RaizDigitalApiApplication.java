3c9cef41d8b072ab095d1a19c8e55dea2484572b
Antonio-avsp <antoniovinicius878@gmail.com>
2025-12-09 08:29:08 -0300

    Add files via upload

---
+++ raiz-digital-api/P.I-main/src/main/java/com/raizdigital/api/RaizDigitalApiApplication.java
@@ -0,0 +1,13 @@
+package com.raizdigital.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizDigitalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaizDigitalApiApplication.class, args);
+    }
+
+}
